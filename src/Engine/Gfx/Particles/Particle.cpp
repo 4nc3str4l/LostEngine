f19927fa7d24f0abb0b1dc6107a6a2c870b8e441
4nc3str4l <cmuri_10@hotmail.com>
2017-11-06 23:00:51 +0100

    Particle Class

--- src/Engine/Gfx/Particles/Particle.cpp
+++ src/Engine/Gfx/Particles/Particle.cpp
@@ -1,1 +1,32 @@
 #include "Particle.h"
+#include "../../Tools/Timer.h"
+namespace LostEngine { namespace Gfx {
+Particle::Particle(vec3* _pos, vec3* _vel, float _grav, float _life, float _rotation, float _scale)
+{
+    position = _pos;
+    rotation = _rotation;
+    scale = _scale;
+    m_velocity = _vel;
+    m_gravityEffect = _grav;
+    m_lifeLength = _life; 
+    
+    m_elapsedTime = 0.0f;
+    m_instantVelocity = new vec3();
+}
+
+Particle::~Particle()
+{
+    delete m_position;
+    delete m_velocity;
+}
+
+bool Particle::Tick()
+{
+    m_velocity.y = -9.8 * m_gravityEffect * Timer::DeltaTime;
+    m_instantVelocity = m_velocity * Timer::DeltaTime;
+    position += m_instantVelocity;
+    m_elapsedTime += Timer::DeltaTime;
+    return m_elapsedTime < m_lifeLength;
+}
+
+}}
--- src/Engine/Gfx/Particles/Particle.h
+++ src/Engine/Gfx/Particles/Particle.h
@@ -1,1 +1,27 @@
 #pragma once
+#include "../Utils/glad.h"
+#include <glm/glm.hpp>
+
+namespace LostEngine { namespace Gfx {
+using namespace glm;
+
+class Particle
+{
+public:
+    Particle(vec3* _pos, vec3* _vel, float _grav, float _life, float _rotation, float _scale);
+    ~Particle();
+    bool Tick();
+public:
+    vec3* position;
+    float rotation;
+    float scale;
+private:
+    vec3* m_velocity;
+    vec3* m_instantVelocity;
+    float m_gravityEffect;
+    float m_lifeLength;
+    
+    float m_elapsedTime;
+};
+
+}}
