8008727612fe9fb4636b7f32828a338312e160d1
4nc3str4l <cmuri_10@hotmail.com>
2018-03-14 23:26:44 +0100

    2D Shader

--- assets/resources/shaders/2d/sprite_renderer.fs
+++ assets/resources/shaders/2d/sprite_renderer.fs
@@ -4,8 +4,11 @@ out vec4 color;
 
 uniform sampler2D image;
 uniform vec3 spriteColor;
+uniform float transparency;
 
 void main()
 {    
-    color = vec4(spriteColor, 1.0) * texture(image, TexCoords);
+    color = vec4(spriteColor, 1.0f) * texture(image, TexCoords);
+    color.w = transparency;
+    color.x = 1.0f;
 } 
