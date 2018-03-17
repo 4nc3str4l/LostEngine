76d9b367733585759b316dacaa45e41da377ca44
4nc3str4l <cmuri_10@hotmail.com>
2018-03-17 04:00:09 +0100

    Packet class

---
+++ src/Engine/Net/Packet.cpp
@@ -0,0 +1,36 @@
+/* Copyright 2016 Guillem Pascual */
+
+#include "packet.h"
+
+Packet::Packet() :
+	_read(0),
+	_size(0),
+	_write(0),
+	_refs(0)
+{}
+
+Packet::~Packet()
+{
+}
+
+
+template <>
+float Packet::read()
+{
+	float f;
+	uint32_t u = read<uint32_t>();
+	memcpy(&f, &u, sizeof(float));
+	return f;
+}
+
+template <>
+glm::vec2 Packet::read()
+{
+	return{ read<float>(), read<float>() };
+}
+
+template <>
+glm::vec3 Packet::read()
+{
+	return{ read<float>(), read<float>(), read<float>() };
+}
