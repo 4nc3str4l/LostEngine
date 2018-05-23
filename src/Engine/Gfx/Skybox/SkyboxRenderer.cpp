b22c7f14c38bf37ca7f65d5442f3f0c31bc8acaf
4nc3str4l <murielmaths@gmail.com>
2018-05-23 14:01:17 +0200

    Fixed Skybox Problems on AMD Cards

--- src/Engine/Gfx/Skybox/SkyboxRenderer.cpp
+++ src/Engine/Gfx/Skybox/SkyboxRenderer.cpp
@@ -73,12 +73,17 @@ namespace le { namespace gfx {
 
 	void SkyboxRenderer::Render(Camera* _camera, Window* _window, const glm::mat4& _projection)
 	{
+		glDepthMask(GL_FALSE);
+	
 		skyboxShader->Use();
 		skyboxShader->LoadViewMatrix(_camera->GetViewMatrix());
+
 		glBindVertexArray(cube->VaoID);
 		glEnableVertexAttribArray(0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
 		glActiveTexture(GL_TEXTURE0);
 		glDrawArrays(GL_TRIANGLES, 0, cube->VertexCount);
+
+		glDepthMask(GL_TRUE);
 	}
 }}
