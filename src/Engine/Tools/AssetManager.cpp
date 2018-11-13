694d34fd5a3143e41e5a4e3757b394597e70ceff
4nc3str4l <muriel.maths@gmail.com>
2018-11-13 14:19:00 +0100

    Using shared poiter for assets

--- src/Engine/Tools/AssetManager.cpp
+++ src/Engine/Tools/AssetManager.cpp
@@ -5,14 +5,14 @@
 
 namespace le { namespace tools {
 
-map<std::string, gfx::Model*> AssetManager::loadedModels;
+map<std::string, std::shared_ptr<gfx::Model>> AssetManager::loadedModels;
 
-gfx::Model* AssetManager::LoadModel(std::string _path) 
+std::shared_ptr<gfx::Model> AssetManager::LoadModel(std::string _path) 
 {
-	gfx::Model* toReturn;
+	std::shared_ptr<gfx::Model> toReturn;
 
 	// Try to find the model on the map
-	map<std::string, gfx::Model*>::iterator it = loadedModels.find(_path);
+	map<std::string, std::shared_ptr<gfx::Model>>::iterator it = loadedModels.find(_path);
 	if (it != loadedModels.end())
 	{
 		toReturn = it->second;
@@ -20,16 +20,15 @@ gfx::Model* AssetManager::LoadModel(std::string _path)
 	// If not on the map just load the model
 	else 
 	{
-		toReturn = new gfx::Model(_path);
-		loadedModels[_path] = toReturn;
+		std::shared_ptr<gfx::Model> m(new gfx::Model(_path));
+		toReturn = m;
+		loadedModels[_path] = m;
 	}
 	return toReturn;
 }
 
 void AssetManager::Dispose()
 {
-	for (std::map<std::string, gfx::Model*>::iterator it = loadedModels.begin(); it != loadedModels.end(); ++it)
-		delete it->second;
 	LOG_CORRECT("Asset Manager Disposed");
 }
 
--- src/Engine/Tools/AssetManager.h
+++ src/Engine/Tools/AssetManager.h
@@ -1,15 +1,17 @@
 #pragma once
+
 #include<map>
 #include<string>
 #include "../Gfx/Models/Model.h"
+#include <memory>
 
 namespace le { namespace tools {
 
 	class AssetManager
 	{
 	public:
-		static std::map <std::string, gfx::Model*> loadedModels;
-		static gfx::Model* LoadModel(std::string _path);
+		static std::map <std::string, std::shared_ptr<gfx::Model>> loadedModels;
+		static std::shared_ptr<gfx::Model> LoadModel(std::string _path);
 		static void Dispose();
 	};
 }}
